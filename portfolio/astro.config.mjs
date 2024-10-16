ed483a01e29defc4e5933aa56439cf282bf35643
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-10-16 06:26:54 +0100

    .

--- portfolio/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import preact from '@astrojs/preact'
-export default defineConfig({
-  integrations: [
-    preact({compat : true}),
-    mdx()
-  ],
-})
--- portfolio/src/component/Text.js
+++
@@ -1,4 +0,0 @@
-import {text} from "ziko"
-export default function Text(){
-    return text("Hello from zikojs component\n")
-}
